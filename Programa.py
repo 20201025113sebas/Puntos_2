110d9bd141ebd00fd357e615eb82dd4aac8afc4d
Sebastian Andres Trigos <112731508+20201025113sebas@users.noreply.github.com>
2022-11-23 17:52:15 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,21 @@
+from Puntos import Punto
+
+messi= Punto()
+elbicho=Punto()
+
+a= messi.Calcular_distancia(elbicho)
+
+messi =Punto()
+elbicho =Punto()
+
+a = messi.Calcular_distancia(elbicho)
+
+messi.x = 8
+messi.y = 8
+
+elbicho.x =9
+elbicho.y =9
+
+a= messi.Calcular_distancia(elbicho)
+
+print (a)
---
+++ Puntos.py
@@ -0,0 +1,15 @@
+import math
+
+class Punto:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        
+def Calcular_distancia(self,otropunto):
+    
+    
+    r= math.sqrt(math.pow((self.x-otropunto),2)+math.pow(self.y-otropunto),2)
+    
+    return r
+
+
